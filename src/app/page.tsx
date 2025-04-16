202a5e1f9833233caebc39d7748fb3139bb19e23
Raunak Chaudhary <raunakc39@gmail.com>
2025-04-16 15:46:25 +0530

    add metatag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,13 @@ import AboutUs from "./components/AboutUs";
 import Review from "./components/Review";
 import BecomeTutor from "./components/BecomeTutor";
 import Footer from "./components/Footer";
-import Head from "next/head";
+
+
 
 export default function Home() {
   return (
     <>
-     <Head>
-        <title>Tutlabs India</title>
-        <meta name="description" content="Default Title for my website" />
-      </Head>
+    
       <Navbar />
       <HeroSection />
       <AboutUs />
